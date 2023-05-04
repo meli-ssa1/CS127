0cb3e6a18c3599bde41d9586df014ef4e25cd700
meli-ssa1 <128660440+meli-ssa1@users.noreply.github.com>
2023-05-04 12:24:33 -0400

    Create 53 Hello World(C++).cpp

---
+++ 53 Hello World(C++).cpp
@@ -0,0 +1,14 @@
+#Name: Melissa Suarez-Sanchez
+#Email: melissa.suarezsanchez676@myhunter.cuny.edu
+#Date: May 4,2023
+#This program displays Hello World(C++). My first program in C++.
+
+#include <iostream>        
+using namespace std;       
+
+int main()                 
+{
+  cout << "Hello, World!\n"; 
+  cout << "C++ is fun!";
+  return 0;                
+}
